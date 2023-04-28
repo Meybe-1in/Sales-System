f8b7e3e0bea54036eb3562a1a553f719bb7d66b2
Dayan <dayanithaclaros@hotmail.com>
2023-04-27 22:14:30 -0600

    rowa charge

--- view/dashboard.php
+++ view/dashboard.php
@@ -20,6 +20,32 @@
     <div class="content">
       <div class="container-fluid">
         <!-- /.row -->
+         <!-- create rows -->
+      <div class="row">
+        
+        <div class="col-lg-2">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+
+        </div>
+          <div class="col-lg-2"></div>
+          <div class="col-lg-2"></div>
+          <div class="col-lg-2"></div>
+          <div class="col-lg-2"></div>
+          <div class="col-lg-2"></div>
+          
+
+      </div>
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
